ca5a160d08876b4e98bb0dd7c8eecb5af9f6e6c8
regonfoong <regonfoong@gmail.com>
2026-06-02 13:18:39 +1000

    pause resume

--- Assets/Scripts/Systems/Restart.cs
+++ Assets/Scripts/Systems/Restart.cs
@@ -1,9 +1,11 @@
+using System.Security.AccessControl;
 using UnityEngine;
 
 public class Restart : MonoBehaviour
 {
     private PlayerInputState _input;
     private PlayerInputState _pause;
+    public GameObject pauseMenu; // Reference to the pause menu UI
     private bool isPaused = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -31,20 +33,50 @@ public class Restart : MonoBehaviour
         if (_pause.PausePressedThisFrame)
         {
             //Toggle pause state
-            isPaused = !isPaused;
-            if (!isPaused)
-            {
-                Debug.Log("Resuming game...");
-                Time.timeScale = 1f; // Resume the game
-            }
-
-            if (isPaused)
-            {
-                Debug.Log("Pausing game...");
-                Time.timeScale = 0f; // Pause the game
-            }
+            TogglePause();
         }
 
+    }
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+        if (!isPaused)
+        {
+            Debug.Log("Resuming game...");
+            Time.timeScale = 1f; // Resume the game
+            pauseMenu.SetActive(false); // Hide the pause menu
+        }
+
+        if (isPaused)
+        {
+            Debug.Log("Pausing game...");
+            Time.timeScale = 0f; // Pause the game
+            Cursor.visible = true; // Lock the cursor
+
+            pauseMenu.SetActive(true); // Show the pause menu
+
+        }
+    }
+    
+    public void Resume()
+    {
+
+        isPaused = false;
+
+        pauseMenu.SetActive(false);
+
+        Time.timeScale = 1f;
+
+        Cursor.visible = false;
+
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
 
