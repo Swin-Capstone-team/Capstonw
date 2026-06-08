19cfca25868a746d43f3fe2e540719b9a7e9590f
regonfoong <regonfoong@gmail.com>
2026-06-08 14:29:18 +1000

    button animation

--- Assets/Scripts/UI/MenuButtons.cs
+++ Assets/Scripts/UI/MenuButtons.cs
@@ -4,6 +4,7 @@ public class MenuButtons : MonoBehaviour
 {
     public MenuController pauseScript; // Reference to the MenuController script
     public GameObject pauseMenu; // Reference to the pause menu UI
+    public Animator buttonAnimator; // Reference to the Animator component for button animations
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
